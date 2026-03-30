7127164f87823866a5aaedd391a88a27406809c1
DanielAlves <daniel_f_alves@estudante.sesisenai.org.br>
2026-03-30 12:53:51 -0300

    up - calculadora basica completa

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,31 @@
 function soma() {
-    let N1 = parseFloat(Document.getElementById("N1").value);
-    let N2 = parseFloat(Document.getElementById("N2").value);
+    let numero1 = parseFloat(document.getElementById("N1").value);
+    let numero2 = parseFloat(document.getElementById("N2").value);
 
-    let resul = N1 + N2
+    let resul = numero1 + numero2
+    alert(resul);
+}
+
+function multi() {
+    let numero1 = parseFloat(document.getElementById("N1").value);
+    let numero2 = parseFloat(document.getElementById("N2").value);
+
+    let resul = numero1 * numero2
+    alert(resul);
+}
+
+function menos() {
+    let numero1 = parseFloat(document.getElementById("N1").value);
+    let numero2 = parseFloat(document.getElementById("N2").value);
+
+    let resul = numero1 - numero2
+    alert(resul);
+}
+
+function divi() {
+    let numero1 = parseFloat(document.getElementById("N1").value);
+    let numero2 = parseFloat(document.getElementById("N2").value);
+
+    let resul = numero1 / numero2
+    alert(resul);
 }
